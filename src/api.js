abea638e76f0f8a314bb625ab32a07921b2eb8e8
Yeraldin3139 <sara.araujo@campusucc.edu.co>
2025-11-13 21:54:40 -0500

    update api base

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = "https://truck-expenses-and-income-backend-1.onrender.com/api";
 
 export const api = axios.create({
   baseURL,
